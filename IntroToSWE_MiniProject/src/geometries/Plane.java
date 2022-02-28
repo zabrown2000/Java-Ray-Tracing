2d5f6db9908740f438078c1be8011c9dc0f9c67d
zabrown2000 <zabrown2000@gmail.com>
2022-02-28 12:22:09 +0200

    added plane

--- IntroToSWE_MiniProject/src/geometries/Plane.java
+++ IntroToSWE_MiniProject/src/geometries/Plane.java
@@ -1,5 +1,51 @@
 package geometries;
 
-public class Plane implements Geometry {
+import primitives.Point;
+import primitives.Vector;
 
+public class Plane implements Geometry {
+	
+	public Point q0;
+	public Vector normal;
+	
+	/**
+	 * Constructor for Plane that takes 3 points
+	 * @param p1 point 1
+	 * @param p2 point 2
+	 * @param p3 point 3
+	 */
+	public Plane(Point p1, Point p2, Point p3) {
+		this.normal = null;
+		this.q0 = p1;
+	}
+	
+	/**
+	 * Constructor for Plane that takes a point and a vector
+	 * @param p point for plane
+	 * @param v normal vector to plane
+	 */
+	public Plane(Point p, Vector v) {
+		this.q0 = p;
+		this.normal = v.normalize();
+	}
+	
+	/**
+	 * Method to get vector normal to the plane
+	 * @return the normal vector
+	 */
+	public Vector getNormal() {
+		return null;
+	}
+	
+	public Point getQ0() {
+		return q0;
+	}
+	
+	@Override
+	public String toString() {
+		return "Plane [q0=" + q0.toString() + ", normal=" + normal.toString() + "]";
+	}
+	
+	
 }
+
