1c131a5d73e1da8bdb0fbe1e8ef284c639be145c
zabrown2000 <zabrown2000@gmail.com>
2022-02-28 12:21:43 +0200

    added ray class

--- IntroToSWE_MiniProject/src/primitives/Ray.java
+++ IntroToSWE_MiniProject/src/primitives/Ray.java
@@ -1,5 +1,48 @@
 package primitives;
 
 public class Ray {
+	
+	public Point p0;
+	public Vector dir;
+	
+	//need 1 ctor (noralize), getters, equals and tostring
+	
+	/**
+	 * constructor to create a Ray
+	 * @param p point field
+	 * @param v vector field
+	 */
+	public Ray(Point p, Vector v) {
+		this.p0 = p;
+		this.dir = v.normalize();
+	}
 
+	/**
+	 * getter method for p0 field
+	 * @return Ray's point field
+	 */
+	public Point getP0() {
+		return p0;
+	}
+
+	/**
+	 * getter method for dir field
+	 * @return Ray's vector field
+	 */
+	public Vector getDir() {
+		return dir;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) return true;
+		if (obj == null) return false;
+		if (!(obj instanceof Ray)) return false;
+		Ray other = (Ray)obj;
+		return this.p0.equals(other.p0) && this.dir.equals(other.dir); 
+	}
+	
+	@Override
+	public String toString() {return p0.toString() + "->" + dir.toString(); }
+	
 }
