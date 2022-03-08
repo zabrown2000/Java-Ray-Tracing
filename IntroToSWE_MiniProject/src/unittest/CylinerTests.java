48b38a57a397c103d51933f04f92488254481641
zabrown2000 <zabrown2000@gmail.com>
2022-03-08 12:58:09 +0200

    added cylinder test doc

---
+++ IntroToSWE_MiniProject/src/unittest/CylinerTests.java
@@ -0,0 +1,17 @@
+package unittest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CylinerTests {
+
+	/**
+	 * Test method for {@link geometries.Cylinder#getNormal(primitives.Point)}.
+	 */
+	@Test
+	void testGetNormal() {
+		fail("Not yet implemented");
+	}
+
+}
