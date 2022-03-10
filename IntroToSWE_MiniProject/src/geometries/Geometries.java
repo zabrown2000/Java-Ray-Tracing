c4f8aa1c8cee67ef788076339271751038eacbe1
zabrown2000 <zabrown2000@gmail.com>
2022-03-10 10:43:25 +0200

    test

--- IntroToSWE_MiniProject/src/geometries/Geometries.java
+++ IntroToSWE_MiniProject/src/geometries/Geometries.java
@@ -6,7 +6,7 @@ import primitives.Point;
 import primitives.Ray;
 import primitives.Vector;
 
-public class Geometries implements Intersectable { //look up composite design pattern
+public class Geometries implements Intersectable { //look up composite design patterns
 
 	public Vector getNormal(Point point) {
 		// TODO Auto-generated method stub
