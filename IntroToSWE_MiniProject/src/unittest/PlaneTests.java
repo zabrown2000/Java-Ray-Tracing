0c731ac440255b8853174bf26c4d6bbf6a8c79f0
Gabi Bondi <gbondi121@gmail.com>
2022-03-14 19:03:18 +0200

    added tests cases for plane

--- IntroToSWE_MiniProject/src/unittest/PlaneTests.java
+++ IntroToSWE_MiniProject/src/unittest/PlaneTests.java
@@ -40,24 +40,50 @@ class PlaneTests {
 	@Test
 	void testFindIntersection() {
 		
-		Plane plane1 = new Plane(new Point(1,1,0), new Vector(1,1,1));
+		
 		// ============ Equivalence Partitions Tests ==============
+		
 		//TC01: Ray intersects the plane 
-		Point p1 = new Point()
-		//TC02: Ray does not intersect the plane 
-		//TC03: Ray orthogonal to the point 
-		//TC04: Ray parallel to the plane
+		Plane plane1 = new Plane(new Point(1,1,0), new Vector(1,1,1));
+		Ray ray1 = new Ray(new Point (0,0,1), new Vector (1,1,0));
+		Point p1 = new Point(0.5,0.5,1);
+		assertEquals(p1, plane1.findIntsersections(ray1), "Ray does not interset the plane");
+		
+		//TC02: Ray does not intersect the plane
+		Plane plane2 = new Plane(new Point(0,0,0), new Vector(0,0,1));
+		Ray ray2 = new Ray(new Point (0,0,1), new Vector (0,2,2));
+		assertNull( plane2.findIntsersections(ray2),"Ray's line out of sphere");
+
 		
 		// =============== Boundary Values Tests ==================
+		
 		//TC11: Ray is parallel to the plane including the plane
+		Plane plane3 = new Plane(new Point(1,1,0), new Vector(1,1,1));
+		Ray ray3 = new Ray(new Point (1,0,0), new Vector (2,2,0));
+		Point p3 = new Point(1.5,0.5,0);
+		assertEquals(p3, plane3.findIntsersections(ray3), "Ray does not interset the plane");
+		
+		
 		//TC12: Ray parallel to the plane excluding the plane
+		Ray ray4 = new Ray(new Point (0,0,4), new Vector (0,4,4));
+		assertNull( plane1.findIntsersections(ray4),"Ray's parallel to the plane excluding the plane ");
+
+		
 		//TC13: orthogonal to the plane before the plane
-		//TC14: orthogonal to the plane in the plane 
+		
+		//TC14: orthogonal to the plane in the plane
+		//asserEquals(p6, plane3.findIntsersections(ray6); orthog in the plane)
+		
 		//TC15: orthogonal to the plane after the plane
+		
 		//TC16: Ray begins at the plane
+		Ray ray8 = new Ray(new Point (0.5,0.5,0), new Vector (0.5,0.5,0.5));
+		Point p8 = new Point(1,1,0.5);
+		assertEquals(p8, plane3.findIntsersections(ray8), "Ray start on the plane");
+		
 		//TC17: Ray begins at the same point with is a reference point in the plane 
-		
-		
+		Ray ray9 = new Ray(new Point (1,1,0), new Vector (1,1,1));
+		assertEquals(new Point(1,1,0), plane3.findIntsersections(ray9), "Ray and refrance point pn the plane begin at the same point");
 		
 		
 	}
