cf4863093cebe0b5941521ef757e92a79cfc8e22
zabrown2000 <zabrown2000@gmail.com>
2022-03-14 15:20:17 +0200

    adding functions to class Geometries

--- IntroToSWE_MiniProject/src/geometries/Geometries.java
+++ IntroToSWE_MiniProject/src/geometries/Geometries.java
@@ -1,5 +1,6 @@
 package geometries;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import primitives.Point;
@@ -7,7 +8,40 @@ import primitives.Ray;
 import primitives.Vector;
 
 public class Geometries implements Intersectable { //look up composite design patterns, testing number of points
+	
+	private List<Intersectable> pointsList;
 
+	/**
+	 * default constructor for pointsList
+	 */
+	public Geometries() {
+		//choosing arrayList because we'll be doing a lot of accessing in the list
+		//and arrayList has O(1) access time
+		pointsList = new ArrayList<Intersectable>();
+	}
+	
+	/**
+	 * constructor to fill in the empty pointsList arrayList
+	 * @param geometries array of intersectable points
+	 */
+	public Geometries(Intersectable... geometries) {
+		
+		for (int i = 0; i < geometries.length; i++) {
+			pointsList.add(geometries[i]);
+		}
+	}
+
+	/**
+	 * function to add a number of objects to the pointsList collection
+	 * @param geometries array of intersectable points
+	 */
+	public void add(Intersectable... geometries) {
+		
+		for (int i = 0; i < geometries.length; i++) {
+			pointsList.add(geometries[i]);
+		}
+	}
+	
 	public List<Point> findIntsersections(Ray ray) {
 		// TODO Auto-generated method stub
 		return null;
