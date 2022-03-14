9a1b2d9d46464a56dc9ee7e905756be21ed26720
zabrown2000 <zabrown2000@gmail.com>
2022-03-14 15:29:04 +0200

    created Geometries junit class

---
+++ IntroToSWE_MiniProject/src/unittest/GeometriesTest.java
@@ -0,0 +1,25 @@
+package unittest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GeometriesTest {
+
+	/**
+	 * Test method for {@link geometries.Geometries#findIntsersections(primitives.Ray)}.
+	 */
+	@Test
+	void testFindIntsersections() {
+		/*an empty collection (BVA)
+		no shape intersects with a body (BVA)
+		only one shape intersects (BVA)
+		some shapes but not all intersects (EP)
+		all shapes intersects (BVA)
+	In the tests above there is no need to check the value of the points 
+	but rather to count the number of intersection points that were found */
+		
+		fail("Not yet implemented");
+	}
+
+}
