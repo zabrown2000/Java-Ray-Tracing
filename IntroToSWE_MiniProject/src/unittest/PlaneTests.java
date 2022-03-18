fb5ec1f9df01fdea42a7867bc05eb30048038b86
Gabi Bondi <gbondi121@gmail.com>
2022-03-18 11:45:33 +0200

    completed plane

--- IntroToSWE_MiniProject/src/unittest/PlaneTests.java
+++ IntroToSWE_MiniProject/src/unittest/PlaneTests.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import geometries.*;
 import primitives.*;
 import java.util.List;
+import java.util.ArrayList;
 
 class PlaneTests {
 
@@ -48,7 +49,9 @@ class PlaneTests {
 		Plane plane1 = new Plane(new Point(1,1,0), new Vector(1,1,1));
 		Ray ray1 = new Ray(new Point (0,0,1), new Vector (1,1,0));
 		Point p1 = new Point(0.5,0.5,1);
-		assertEquals(p1, plane1.findIntsersections(ray1), "Ray does not interset the plane");
+		List<Point> list1 = new ArrayList<>();
+		list1.add(p1);
+		assertEquals(list1, plane1.findIntsersections(ray1), "Ray does not interset the plane");
 		
 		//TC02: Ray does not intersect the plane
 		Plane plane2 = new Plane(new Point(0,0,0), new Vector(0,0,1));
@@ -62,23 +65,27 @@ class PlaneTests {
 		Plane plane3 = new Plane(new Point(1,1,0), new Vector(1,1,1));
 		Ray ray3 = new Ray(new Point (1,0,0), new Vector (2,2,0));
 		Point p3 = new Point(1.5,0.5,0);
-		assertEquals(p3, plane3.findIntsersections(ray3), "Ray does not interset the plane");
+		List<Point> list3 = new ArrayList<>();
+		list3.add(p3);
+		assertEquals(list3, plane3.findIntsersections(ray3), "Ray does not interset the plane");
 		
 		
 		//TC12: Ray parallel to the plane excluding the plane
 		Ray ray4 = new Ray(new Point (0,0,4), new Vector (0,4,4));
-		assertNull( plane1.findIntsersections(ray4),"Ray's parallel to the plane excluding the plane ");
+	    assertNull( plane1.findIntsersections(ray4),"Ray's parallel to the plane excluding the plane ");
 
 		
 		//TC13: orthogonal to the plane before the plane
 		//interpreting before below the plane as the plane is infinitly long and wide 
 		Ray ray5 = new Ray(new Point(1,1,-1), new Vector(1,1,0));
-		Point p5 = new Point (1,1,0);
-		assertEquals(p5, plane3.findIntsersections(ray5), "Ray orthogonal to the plane before the plane");
+		Point p5 = new Point (1.5,1.5,-1);
+		List<Point> list5 = new ArrayList<>();
+		list5.add(p5);
+		assertEquals(list5, plane3.findIntsersections(ray5), "Ray orthogonal to the plane before the plane");
 		
 		//TC14: orthogonal to the plane in the plan
 		//ray starts on the plane 
-		assertEquals(p5, plane3.findIntsersections(ray5), "Ray orthogonal to the plane in the plane");
+		assertEquals(list5, plane3.findIntsersections(ray5), "Ray orthogonal to the plane in the plane");
 				
 		//TC15: orthogonal to the plane after the plan
 		// ray starts below after the plane and therefore should not intersect 
@@ -88,13 +95,14 @@ class PlaneTests {
 		
 		//TC16: Ray begins at the plane
 		Ray ray8 = new Ray(new Point (0.5,0.5,0), new Vector (0.5,0.5,0.5));
-		Point p8 = new Point(1,1,0.5);
-		assertEquals(p8, plane3.findIntsersections(ray8), "Ray start on the plane");
+		Point p8 = new Point(0.8333333333333334,0.8333333333333334,0.33333333333333337);
+		List<Point> list8 = new ArrayList<>();
+		list8.add(p8);
+		assertEquals(list8, plane3.findIntsersections(ray8), "Ray start on the plane");
 		
 		//TC17: Ray begins at the same point with is a reference point in the plane 
-		Ray ray9 = new Ray(new Point (1,1,0), new Vector (1,1,1));
-		assertEquals(new Point(1,1,0), plane3.findIntsersections(ray9), "Ray and refrance point pn the plane begin at the same point");
-		
+		Ray ray9 = new Ray(new Point (1,1,0), new Vector (6,6,1));
+		 assertThrows(IllegalArgumentException.class, () -> plane3.findIntsersections(ray9),"Ray begins at the same point with is a reference point in the plane ");
 		
 	}
 }
--- IntroToSWE_MiniProject/src/unittest/SphereTests.java
+++ IntroToSWE_MiniProject/src/unittest/SphereTests.java
@@ -45,32 +45,32 @@ class SphereTests {
         // ============ Equivalence Partitions Tests ==============
 
         // TC01: Ray's line is outside the sphere (0 points)
-        assertNull(sphere.findIntersections(new Ray(new Point(-1, 0, 0), new Vector(1, 1, 0))),"Ray's line out of sphere");
+        assertNull(sphere.findIntsersections(new Ray(new Point(-1, 0, 0), new Vector(1, 1, 0))),"Ray's line out of sphere");
 
         // TC02: Ray starts before and crosses the sphere (2 points)
         Point p1 = new Point(0.0651530771650466, 0.355051025721682, 0);
         Point p2 = new Point(1.53484692283495, 0.844948974278318, 0);
-        List<Point> result = sphere.findIntersections(new Ray(new Point(-1, 0, 0),new Vector(3, 1, 0)));
+        List<Point> result = sphere.findIntsersections(new Ray(new Point(-1, 0, 0),new Vector(3, 1, 0)));
         assertEquals( 2, result.size(),"Wrong number of points");
         if (result.get(0).getX() > result.get(1).getX())
             result = List.of(result.get(1), result.get(0));
         assertEquals(List.of(p1, p2), result, "Wrong number of points");
 
         // TC03: Ray starts inside the sphere (1 point)
-        assertEquals(new Point(1,0,1), sphere.findIntersections(new Ray(new Point(1, 0, 0),new Vector(1, 0, 1))),"ray starts inside the sphere"); 
+        assertEquals(new Point(1,0,1), sphere.findIntsersections(new Ray(new Point(1, 0, 0),new Vector(1, 0, 1))),"ray starts inside the sphere"); 
        
         // TC04: Ray starts after the sphere (0 points)
-        assertNull(sphere.findIntersections(new Ray (new Point(6,6,6), new Vector(7,7,7))), "ray starts after the sphere");
+        assertNull(sphere.findIntsersections(new Ray (new Point(6,6,6), new Vector(7,7,7))), "ray starts after the sphere");
        
 
         // =============== Boundary Values Tests ==================
 
         // **** Group: Ray's line crosses the sphere (but not the center)
         // TC11: Ray starts at sphere and goes inside (1 points)
-        assertEquals(new Point(1,0,1), sphere.findIntersections(new Ray(new Point (2,0,0), new Vector(1,0,1))), "Ray starts at sphere and goes inside");
+        assertEquals(new Point(1,0,1), sphere.findIntsersections(new Ray(new Point (2,0,0), new Vector(1,0,1))), "Ray starts at sphere and goes inside");
         
         // TC12: Ray starts at sphere and goes outside (0 points)
-        assertNull(sphere.findIntersections(new Ray(new Point(2,0,0), new Vector(2,0,1) )),"Ray starts at sphere and goes outside"); //double check might not work
+        assertNull(sphere.findIntsersections(new Ray(new Point(2,0,0), new Vector(2,0,1) )),"Ray starts at sphere and goes outside"); //double check might not work
 
         // **** Group: Ray's line goes through the center
         // TC13: Ray starts before the sphere (2 points)
@@ -79,10 +79,10 @@ class SphereTests {
         // TC16: Ray starts at the center (1 points)
         
         // TC17: Ray starts at sphere and goes outside (0 points)
-        assertNull(sphere.findIntersection(new Ray(new Point(1,1,0), new Vector(0,1,0) )), "Ray starts at sphere and goes outside");
+        assertNull(sphere.findIntsersections(new Ray(new Point(1,1,0), new Vector(0,1,0) )), "Ray starts at sphere and goes outside");
         
         // TC18: Ray starts after sphere (0 points)
-        assertNull(sphere.findIntersections(new Ray(new Point(2,5,0), new Vector(1,5,0)))," Ray starts at sphere and goes outside");
+        assertNull(sphere.findIntsersections(new Ray(new Point(2,5,0), new Vector(1,5,0)))," Ray starts at sphere and goes outside");
 
         // **** Group: Ray's line is tangent to the sphere (all tests 0 points)
         // TC19: Ray starts before the tangent point
