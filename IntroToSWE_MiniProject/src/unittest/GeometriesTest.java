99f69fc950e57de9626c83c7ff7c57c664bb92b6
zabrown2000 <zabrown2000@gmail.com>
2022-03-21 14:52:14 +0200

    fixed null errors, still other issue

--- IntroToSWE_MiniProject/src/unittest/GeometriesTest.java
+++ IntroToSWE_MiniProject/src/unittest/GeometriesTest.java
@@ -20,6 +20,8 @@ class GeometriesTest {
 		
 		Plane p = new Plane(new Point(0,1,-2), new Point(1,-1,2), new Point(-1,-1,2));
 		Triangle t = new Triangle(new Point(0,1,-2), new Point(1,-1,2), new Point(-1,-1,2));
+		//Plane p = new Plane(new Point(0,-3.69,0), new Point(0,0,5), new Point(-5,-2,0));
+		//Triangle t = new Triangle(new Point(-5.53,5.14,3), new Point(2.33,1.82,0), new Point(-0.49,-1.7,2));
 		Sphere s = new Sphere(new Point(0,0,1), 1);
 		
 		Geometries group = new Geometries(p, t, s); 
@@ -28,21 +30,20 @@ class GeometriesTest {
 		//TC01: Some shapes but not all intersect
 		//ray will intersect triangle and plane only -> 2 points
 		Ray r1 = new Ray(new Point(0.5,1,-1), new Vector(-0.5,-3,1));
+		//Ray r1 = new Ray(new Point(-6.43,-3.51,4), new Vector(4.45,6.5,-2.23));
 		assertEquals(2d, group.findIntsersections(r1).size(), "ERROR with some shapes intersecting");
 		
 		// =============== Boundary Values Tests ==================
 		//TC11: Empty collection of shapes
-		//will return null so length is 0
 		//the geos list is empty - create new Geometry with default ctor and just use r1
 		Geometries emptyGroup = new Geometries();
-		assertEquals(0, emptyGroup.findIntsersections(r1).size(), "ERROR with empty collection of shapes");
+		assertEquals(null, emptyGroup.findIntsersections(r1), "ERROR with empty collection of shapes");
 		
 		
 		
 		//TC12: No shape intersects with a body
-		//will return null so length is 0
 		Ray r2 = new Ray(new Point(0.5,1,-1), new Vector(-0.5,1.17,1));
-		assertEquals(0, group.findIntsersections(r2).size(), "ERROR with no shapes intersecting");
+		assertEquals(null, group.findIntsersections(r2), "ERROR with no shapes intersecting");
 		
 		//TC13: Only 1 shape intersects
 		//all but 1 geo returns null
