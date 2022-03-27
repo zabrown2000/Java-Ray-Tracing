37e4745c89fe16dab4b4651d119b6d83e2e5d5a3
zabrown2000 <zabrown2000@gmail.com>
2022-03-27 19:48:17 +0300

    Created camera integration test class

---
+++ IntroToSWE_MiniProject/src/unittest/GeoCameraIntegrationTests.java
@@ -0,0 +1,23 @@
+package unittest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GeoCameraIntegrationTests {
+
+	@Test
+	void testSphereCamera() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testTrangleCamera() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPlaneCamera() {
+		fail("Not yet implemented");
+	}
+}
