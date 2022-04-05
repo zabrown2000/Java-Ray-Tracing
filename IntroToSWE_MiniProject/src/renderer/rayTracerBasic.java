300bc17829dee1fd62412ef51e3886db892b703a
Gabi Bondi <gbondi121@gmail.com>
2022-04-05 11:56:39 +0200

    added ray tracer basic function

--- IntroToSWE_MiniProject/src/renderer/rayTracerBasic.java
+++ IntroToSWE_MiniProject/src/renderer/rayTracerBasic.java
@@ -1,5 +1,24 @@
 package renderer;
+import java.awt.Color;
+import elements.AmbientLight;
+import geometries.*;
+import primitives.*;
+import scene.*;
 
 public class rayTracerBasic extends RayTraceBase {
+	
+	/**
+	 * constructor 
+	 * @param scene Scene
+	 */
+	public rayTracerBasic(Scene scene) { super(scene);}
+	
+	/**
+	 * override traceRay
+	 */
+	@Override
+	public Color traceRay(Ray ray) {
+		return null;
+	}
 
 }
