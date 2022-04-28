b4b0ccf6534f8b0b08ff3f0234de8e04a7e813b0
Gabi Bondi <gbondi121@gmail.com>
2022-04-28 10:43:09 +0300

    added tests

--- IntroToSWE_MiniProject/src/unittests/RenderTests.java
+++ IntroToSWE_MiniProject/src/unittests/RenderTests.java
@@ -1,12 +1,13 @@
-package unittests;
+package unittests.renderer;
 
 import org.junit.jupiter.api.Test;
+
 import lighting.AmbientLight;
 import geometries.*;
-import lighting.AmbientLight;
 import primitives.*;
 import renderer.*;
 import scene.Scene;
+import static java.awt.Color.*;
 
 /**
  * Test rendering a basic image
@@ -22,43 +23,49 @@ public class RenderTests {
 	@Test
 	public void basicRenderTwoColorTest() {
 		Scene scene = new Scene("Test scene")//
-				.setAmbientLight(new AmbientLight(new Color(255, 191, 191), new Double3(1d,1d,1d))) 
+				.setAmbientLight(new AmbientLight(new Color(255, 191, 191), //
+						new Double3(1, 1, 1))) //
 				.setBackground(new Color(75, 127, 90));
 
-		scene.geometries.add(new Sphere(new Point(0, 0, -100), 50),
+		scene.geometries.add(new Sphere(new Point(0, 0, -100), 50d),
 				new Triangle(new Point(-100, 0, -100), new Point(0, 100, -100), new Point(-100, 100, -100)), // up
-																													// left
+																												// left
 				new Triangle(new Point(-100, 0, -100), new Point(0, -100, -100), new Point(-100, -100, -100)), // down
-																														// left
+																												// left
 				new Triangle(new Point(100, 0, -100), new Point(0, -100, -100), new Point(100, -100, -100))); // down
-																													// right
+																												// right
 		Camera camera = new Camera(Point.ZERO, new Vector(0, 0, -1), new Vector(0, 1, 0)) //
 				.setVPDistance(100) //
 				.setVPSize(500, 500) //
-				.setImageWriter(new ImageWriter("base render test", 1000, 1000))				
+				.setImageWriter(new ImageWriter("base render test", 1000, 1000))
 				.setRayTracer(new RayTracerBasic(scene));
 
 		camera.renderImage();
-		camera.printGrid(100, new Color(java.awt.Color.YELLOW));
+		camera.printGrid(100, new Color(YELLOW));
 		camera.writeToImage();
 	}
-	
+
+	// For stage 6 - please disregard in stage 5
+	/**
+	 * Produce a scene with basic 3D model - including individual lights of the
+	 * bodies and render it into a png image with a grid
+	 */
 	@Test
 	public void basicRenderMultiColorTest() {
 		Scene scene = new Scene("Test scene")//
-				.setAmbientLight(new AmbientLight(new Color(java.awt.Color.WHITE), new Double3(0.2))); //
+				.setAmbientLight(new AmbientLight(new Color(WHITE), new Double3(0.2))); //
 
 		scene.geometries.add( //
 				new Sphere(new Point(0, 0, -100), 50),
 				// up left
 				new Triangle(new Point(-100, 0, -100), new Point(0, 100, -100), new Point(-100, 100, -100))
-						.setEmission(new Color(java.awt.Color.GREEN)),
+						.setEmission(new Color(GREEN)),
 				// down left
 				new Triangle(new Point(-100, 0, -100), new Point(0, -100, -100), new Point(-100, -100, -100))
-						.setEmission(new Color(java.awt.Color.RED)),
+						.setEmission(new Color(RED)),
 				// down right
 				new Triangle(new Point(100, 0, -100), new Point(0, -100, -100), new Point(100, -100, -100))
-						.setEmission(new Color(java.awt.Color.BLUE)));
+						.setEmission(new Color(BLUE)));
 
 		Camera camera = new Camera(Point.ZERO, new Vector(0, 0, -1), new Vector(0, 1, 0)) //
 				.setVPDistance(100) //
@@ -67,7 +74,7 @@ public class RenderTests {
 				.setRayTracer(new RayTracerBasic(scene));
 
 		camera.renderImage();
-		camera.printGrid(100, new Color(java.awt.Color.WHITE));
+		camera.printGrid(100, new Color(WHITE));
 		camera.writeToImage();
 	}
 
@@ -82,11 +89,10 @@ public class RenderTests {
 
 		Camera camera = new Camera(Point.ZERO, new Vector(0, 0, -1), new Vector(0, 1, 0)) //
 				.setVPDistance(100) //
-				.setVPSize(500, 500)
-				.setImageWriter(new ImageWriter("xml render test", 1000, 1000))
+				.setVPSize(500, 500).setImageWriter(new ImageWriter("xml render test", 1000, 1000))
 				.setRayTracer(new RayTracerBasic(scene));
 		camera.renderImage();
-		camera.printGrid(100, new Color(java.awt.Color.YELLOW));
+		camera.printGrid(100, new Color(YELLOW));
 		camera.writeToImage();
 	}
 }
