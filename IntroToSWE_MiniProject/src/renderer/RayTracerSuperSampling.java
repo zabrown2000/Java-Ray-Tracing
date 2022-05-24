6e454fadadd538490d5ffbda6e5111d9e1c4c769
zabrown2000 <zabrown2000@gmail.com>
2022-05-24 13:39:45 +0300

    added class for super sampling

--- IntroToSWE_MiniProject/src/renderer/RayTracerSuperSampling.java
+++ IntroToSWE_MiniProject/src/renderer/RayTracerSuperSampling.java
@@ -1,1 +1,29 @@
+package renderer;
+import java.awt.Color;
+import java.util.List;
 
+
+import lighting.AmbientLight;
+import lighting.LightSource;
+import geometries.*;
+import lighting.AmbientLight;
+import geometries.Intersectable.GeoPoint;
+import primitives.*;
+import scene.*;
+import geometries.*;
+
+public class RayTracerSuperSampling extends RayTraceBase {
+	
+	/**
+	 * constructor 
+	 * @param scene Scene
+	 */
+	public RayTracerSuperSampling(Scene scene) { super(scene);}
+
+	
+	@Override
+	public primitives.Color traceRay(Ray ray) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
