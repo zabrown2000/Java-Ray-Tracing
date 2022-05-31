15b518629379b98e480e6671d0ec44b20f95cf2c
zabrown2000 <25807259+zabrown2000@users.noreply.github.com>
2022-05-31 13:56:38 +0300

    Add files via upload

---
+++ IntroToSWE_MiniProject/src/unittests/SuperSamplingGlossyTests.java
@@ -0,0 +1,14 @@
+package unittests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SuperSamplingGlossyTests {
+
+	@Test
+	void glossyAndDiffuseTest() {
+		
+	}
+
+}
