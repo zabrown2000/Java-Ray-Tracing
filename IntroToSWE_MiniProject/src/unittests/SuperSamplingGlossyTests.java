f082e15b4e9cefbdd1c43c174347c95d1425fc29
Gabriella21012000 <79445586+Gabriella21012000@users.noreply.github.com>
2022-06-06 10:22:56 +0300

    Update SuperSamplingGlossyTests.java

--- IntroToSWE_MiniProject/src/unittests/SuperSamplingGlossyTests.java
+++ IntroToSWE_MiniProject/src/unittests/SuperSamplingGlossyTests.java
@@ -24,24 +24,26 @@ class SuperSamplingGlossyTests {
 		scene.geometries.add( //
 				//new Sphere(new Point(-550, -500, -1000), 400d).setEmission(new Color(0, 0, 100)) //blue sphere
 				//		.setMaterial(new Material().setKD(0.25).setKS(0.25).setShininess(20).setkT(0.5)),
-				new Sphere(new Point(-550, -500, -1200), 200d).setEmission(new Color(100, 20, 20)) //pink sphere
+				new Sphere(new Point(-550, -500, -1200), 200d).setEmission(new Color(255, 47, 154)) //pink sphere
 						.setMaterial(new Material().setKD(0.6)),
-				new Sphere(new Point(-1300,-100,-300), 300d).setEmission(new Color(20,255,20)) //green sphere, further away
+				new Sphere(new Point(-1300,-100,-300), 200d).setEmission(new Color(23,236,255)) //green sphere, further away
 						.setMaterial(new Material().setKD(0.6)),
-				new Triangle(new Point(1500, -1500, -1500), new Point(-1500, 1500, -1500), new Point(670, 670, 3000)) //tall triangle, mirror
-						.setEmission(new Color(20, 20, 20)) //
-						.setMaterial(new Material().setkR(1.0)),
-				new Triangle(new Point(1500, -1500, -1500), new Point(-1500, 1500, -1500), //layflat triangle
+				//new Triangle(new Point(1500, -1500, -1500), new Point(-1500, 1500, -1500), new Point(670, 670, 3000)) //tall triangle, mirror
+						//.setEmission(new Color(20, 20, 20)) //
+						//.setMaterial(new Material().setkR(1.0)));
+				new Triangle(new Point(1500, -1500, -1500), new Point(-1500, 1500, -1500),
 						new Point(-1500, -1500, -2000)) //
 						.setEmission(new Color(20, 20, 20)) //
 						.setMaterial(new Material().setkR(0.5)));
+						
 
 		scene.lights.add(new SpotLight(new Color(1020, 400, 400), new Point(-750, -750, -150), new Vector(-1, -1, -4)) //
 				.setKL(0.00001).setKQ(0.000005));
 
 		ImageWriter imageWriter = new ImageWriter("superSamplingImprovement", 500, 500);
 		camera.setImageWriter(imageWriter) //
-		        .setRayTracer(new RayTracerSuperSampling(scene));
+		
+		        .setRayTracer(new SuperSampling(scene));
 				camera.renderImage(); //
 				camera.writeToImage();
 	}
